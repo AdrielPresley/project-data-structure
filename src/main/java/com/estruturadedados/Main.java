89f784b77fa1907a0d4c129a520a374a06e70e16
AdrielPresley <adriel_tel11@hotmail.com>
2021-10-14 17:55:10 -0300

    Update

---
+++ src/main/java/com/estruturadedados/Main.java
@@ -0,0 +1,11 @@
+package com.estruturadedados;
+
+public class Main {
+    public Main() {
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println("Aprendendo Estrutura de Dados em Java!");
+    }
+}
---
+++ target/class/com/estruturadedados/Main.java
@@ -0,0 +1,8 @@
+package com.estruturadedados;
+
+public class Main {
+    public static void main(String[] args){
+
+        System.out.println("Aprendendo Estrutura de Dados em Java!");
+    }
+}
