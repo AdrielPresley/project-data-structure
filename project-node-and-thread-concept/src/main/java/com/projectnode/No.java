94cfea2da7a87d8d0313431739bee5d03ce7ab82
AdrielPresley <adriel_tel11@hotmail.com>
2021-10-15 09:18:26 -0300

    Update

--- project-node-and-thread-concept/src/main/java/com/projectnode/No.java
+++ project-node-and-thread-concept/src/main/java/com/projectnode/No.java
@@ -1,4 +1,30 @@
 package com.projectnode;
 
 public class No {
+    private String conteudo;
+    private No proximoNo = null;
+
+    public No(String conteudo) {
+        this.conteudo = conteudo;
+    }
+
+    public String getConteudo() {
+        return this.conteudo;
+    }
+
+    public void setConteudo(String conteudo) {
+        this.conteudo = conteudo;
+    }
+
+    public No getProximoNo() {
+        return this.proximoNo;
+    }
+
+    public void setProximoNo(No proximoNo) {
+        this.proximoNo = proximoNo;
+    }
+
+    public String toString() {
+        return "No{conteudo='" + this.conteudo + "'}";
+    }
 }
