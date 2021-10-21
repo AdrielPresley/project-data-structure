09cc5ffe21e1fe72d358a0fa7e941c779c35792a
AdrielPresley <adriel_tel11@hotmail.com>
2021-10-21 11:58:04 -0300

    Update

---
+++ project-binary-tree/src/main/java/one/dio/model/ObjArvore.java
@@ -0,0 +1,14 @@
+package one.dio.model;
+
+public abstract class ObjArvore<T> implements Comparable<T> {
+    public ObjArvore() {
+    }
+
+    public abstract boolean equals(Object var1);
+
+    public abstract int hashCode();
+
+    public abstract int compareTo(T var1);
+
+    public abstract String toString();
+}
