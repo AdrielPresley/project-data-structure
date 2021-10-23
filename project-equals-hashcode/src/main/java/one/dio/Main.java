eff7b11068542b62de7d50bc40ad2470944a7cbe
AdrielPresley <adriel_tel11@hotmail.com>
2021-10-22 21:13:25 -0300

    Update

--- project-equals-hashcode/src/main/java/one/dio/Main.java
+++ project-equals-hashcode/src/main/java/one/dio/Main.java
@@ -1,4 +1,19 @@
 package one.dio;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
+    public Main() {
+    }
+
+    public static void main(String[] args) {
+        List<Carro> listaCarros = new ArrayList();
+        listaCarros.add(new Carro("Ford"));
+        listaCarros.add(new Carro("Chevrolet"));
+        listaCarros.add(new Carro("Volkswagen"));
+        System.out.println(listaCarros.contains(new Carro("Ford")));
+        System.out.println((new Carro("Ford")).hashCode());
+        System.out.println((new Carro("Ford")).hashCode());
+    }
 }
