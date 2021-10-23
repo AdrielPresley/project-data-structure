04beca86c37eff1d5b0676fa0d2b5d23d81a99ad
AdrielPresley <adriel_tel11@hotmail.com>
2021-10-23 10:28:33 -0300

    Update

---
+++ project-set/src/main/java/one/dio/Carro.java
@@ -0,0 +1,43 @@
+package one.dio;
+
+import java.util.Objects;
+
+public class Carro implements Comparable<Carro> {
+    private String marca;
+
+    public Carro(String marca) {
+        this.marca = marca;
+    }
+
+    public String getMarca() {
+        return this.marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        } else if (!(o instanceof Carro)) {
+            return false;
+        } else {
+            Carro carro = (Carro)o;
+            return Objects.equals(this.getMarca(), carro.getMarca());
+        }
+    }
+
+    public int hashCode() {
+        return Objects.hash(new Object[]{this.getMarca()});
+    }
+
+    public String toString() {
+        return "Carro{marca='" + this.marca + "'}";
+    }
+
+    public int compareTo(Carro o) {
+        return this.getMarca().compareTo(o.getMarca());
+    }
+
+}
