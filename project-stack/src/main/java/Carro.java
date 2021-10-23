1ade8c75e51bbe95c724aafddcf1e8ce8f19bf73
AdrielPresley <adriel_tel11@hotmail.com>
2021-10-22 21:22:49 -0300

    Update

--- project-stack/src/main/java/Carro.java
+++ project-stack/src/main/java/Carro.java
@@ -1,2 +1,36 @@
+import java.util.Objects;
+
 public class Carro {
+    private String marca;
+
+    public Carro(String marca) {
+        this.marca = marca;
+    }
+
+    public String getMarca() {
+        return this.marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        } else if (!(o instanceof Carro)) {
+            return false;
+        } else {
+            Carro carro = (Carro)o;
+            return Objects.equals(this.getMarca(), carro.getMarca());
+        }
+    }
+
+    public int hashCode() {
+        return Objects.hash(new Object[]{this.getMarca()});
+    }
+
+    public String toString() {
+        return "Carro{marca='" + this.marca + "'}";
+    }
 }
